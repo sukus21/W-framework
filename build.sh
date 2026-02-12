e68c0bc76d99dc20216a45cef2fbf855fcacad32
sukus <52664542+sukus21@users.noreply.github.com>
2026-02-12 23:48:26 +0100

    Zip JS files in build script
    I have no idea how Xem managed to get the sizes that low. I must be doing SOMETHING wrong...

--- build.sh
+++ build.sh
@@ -27,3 +27,13 @@ terser w.js \
     --define W.plugin.smooth=false \
     --define W.plugin.builtinShapes=false \
     > w.lite.min.js
+
+# Zip both files
+zip -9 w.full.min.zip w.full.min.js
+zip -9 w.lite.min.zip w.lite.min.js
+
+# Print file sizes
+echo "w.full.min.js:  $(ls -l w.full.min.js | awk '{print $5}') bytes"
+echo "w.lite.min.js:  $(ls -l w.lite.min.js | awk '{print $5}') bytes"
+echo "w.full.min.zip: $(ls -l w.full.min.zip | awk '{print $5}') bytes"
+echo "w.lite.min.zip: $(ls -l w.lite.min.zip | awk '{print $5}') bytes"
