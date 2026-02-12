d7b02c14c2a59eab837ca8d54f433621df8565bd
sukus <52664542+sukus21@users.noreply.github.com>
2026-02-12 20:45:05 +0100

    Remove support for named primitive
    This one needs some rationale. Since the goal of the framework is to be as compact as possible for things like js13k, users writing out the name of the primitive is unlikely, in my opinion. They will either go to the WebGL page and copy the values from there, or use the constant directly, and compile that down to a constant using a home-grown build tool.
    
    Removing support for these saves a few bytes, and makes future typescript bindings easier to make.
    
    This also updates the examples.

--- w.js
+++ w.js
@@ -161,12 +161,12 @@ W = {
     // Save object's type,
     // merge previous state (or default state) with the new state passed in parameter,
     // and reset f (the animation timer)
-    state = {type, ...(W.current[state.n] = W.next[state.n] || {w:1, h:1, d:1, x:0, y:0, z:0, rx:0, ry:0, rz:0, b:'888', mode:4, mix: 0}), ...state, f:0};
+    state = {type, ...(W.current[state.n] = W.next[state.n] || {w:1, h:1, d:1, x:0, y:0, z:0, rx:0, ry:0, rz:0, b:'888', mode:4 /* TRIANGLES */, mix: 0}), ...state, f:0};
     
     // Build the model's vertices buffer if it doesn't exist yet
     if(W.models[state.type]?.vertices && !W.models?.[state.type].verticesBuffer){
       W.gl.bindBuffer(34962 /* ARRAY_BUFFER */, W.models[state.type].verticesBuffer = W.gl.createBuffer());
-      W.gl.bufferData(34962 /* ARRAY_BUFFER */, new Float32Array(W.models[state.type].vertices), 35044 /*STATIC_DRAW*/);
+      W.gl.bufferData(34962 /* ARRAY_BUFFER */, new Float32Array(W.models[state.type].vertices), 35044 /* STATIC_DRAW */);
 
       // Compute smooth normals if they don't exist yet (optional)
       if(!W.models[state.type].normals && W.smooth) W.smooth(state);
@@ -174,7 +174,7 @@ W = {
       // Make a buffer from the smooth/custom normals (if any)
       if(W.models[state.type].normals){
         W.gl.bindBuffer(34962 /* ARRAY_BUFFER */, W.models[state.type].normalsBuffer = W.gl.createBuffer());
-        W.gl.bufferData(34962 /* ARRAY_BUFFER */, new Float32Array(W.models[state.type].normals.flat()), 35044 /*STATIC_DRAW*/); 
+        W.gl.bufferData(34962 /* ARRAY_BUFFER */, new Float32Array(W.models[state.type].normals.flat()), 35044 /* STATIC_DRAW */); 
       }      
     }
     
@@ -368,13 +368,13 @@ W = {
         object.s,
         
         // Enable shading if in TRIANGLE* mode and object.ns disabled
-        ((object.mode > 3) || (W.gl[object.mode] > 3)) && !object.ns ? 1 : 0,
+        object.mode > 3 && !object.ns,
         
         // Ambient light
         W.ambientLight || 0.2,
         
         // Texture/color mix (if a texture is present. 0: fully textured, 1: fully colored)
-        object.mix
+        object.mix,
       );
       
       // If the object is a billboard: send a specific uniform to the shaders:
@@ -408,10 +408,10 @@ W = {
       // Both indexed and unindexed models are supported.
       // You can keep the "drawElements" only if all your models are indexed.
       if(W.models[object.type].indicesBuffer){
-        W.gl.drawElements(+object.mode || W.gl[object.mode], W.models[object.type].indices.length, 5123 /* UNSIGNED_SHORT */, 0);
+        W.gl.drawElements(object.mode, W.models[object.type].indices.length, 5123 /* UNSIGNED_SHORT */, 0);
       }
       else {
-        W.gl.drawArrays(+object.mode || W.gl[object.mode], 0, W.models[object.type].vertices.length / 3);
+        W.gl.drawArrays(object.mode, 0, W.models[object.type].vertices.length / 3);
       }
     }
   },
