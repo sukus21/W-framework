3e8594846edb4e47b4e9d6b37217d79b891d620b
sukus <52664542+sukus21@users.noreply.github.com>
2026-02-24 22:20:48 +0100

    Animation code improvements
    Makes the code footprint smaller, and fixes a couple minor issues:
    
    * User-specified matrices (M parameter) are now accounted for in `W.animation`
    * Nested groups are now more deterministic, especially for deeply nested camera
    * Processing order for objects in groups is now deterministic
      * Previously, objects would jitter, if attached to a group instantiated later than the object
      * Distance to camera is now calculated using current position instead of previous position
    
    compared to previous commit:
    w.min.full.js: 7129 bytes (-192)
    w.min.lite.js: 5039 bytes (-192)
    w.min.full.zip: 2547 bytes (-37)
    w.min.lite.zip: 1952 bytes (-35)

--- w.js
+++ w.js
@@ -245,25 +245,11 @@ W = {
     W.lastFrame = now;
     requestAnimationFrame(W.draw);
     
-    if(W.next.camera.g){
-      W.render(W.next[W.next.camera.g], dt, 1);
-    }
-    
-    // Create a matrix called v containing the current camera transformation
-    v = W.animation('camera');
-    
-    // If the camera is in a group
-    if(W.next.camera.g){
-
-      // premultiply the camera matrix by the group's model matrix.
-      v.preMultiplySelf(W.next[W.next.camera.g].M || W.next[W.next.camera.g].m);
-    }
-    
-    // Send it to the shaders as the Eye matrix
+    // Build camera transformation matrix, and send it to the shaders as the Eye matrix
     W.gl.uniformMatrix4fv(
       W.gl.getUniformLocation(W.program, 'eye'),
       0,
-      v.toFloat32Array(),
+      (v = W.animation('camera')).toFloat32Array(),
     );
     
     // Build and send perspective matrix to the shaders
@@ -286,6 +272,8 @@ W = {
     
     // Render all the objects in the scene
     for(i in W.next){
+      // Update objects model matrix
+      W.next[i].m = W.animation(i);
       
       // Render the shapes with no texture and no transparency (RGB1 color)
       if(!W.next[i].t && W.col(W.next[i].b)[3] == 1){
@@ -342,21 +330,11 @@ W = {
     // ...but don't let it go over the animation duration.
     if(object.f > object.a) object.f = object.a;
 
-    // Compose the model matrix from lerped transformations
-    W.next[object.n].m = W.animation(object.n);
-
-    // If the object is in a group:
-    if(W.next[object.g]){
-
-      // premultiply the model matrix by the group's model matrix.
-      W.next[object.n].m.preMultiplySelf(W.next[object.g].M || W.next[object.g].m);
-    }
-
     // send the model matrix to the vertex shader
     W.gl.uniformMatrix4fv(
       W.gl.getUniformLocation(W.program, 'm'),
       0,
-      (W.next[object.n].M || W.next[object.n].m).toFloat32Array(),
+      object.m.toFloat32Array(),
     );
 
     // Show warning if model doesn't exist (debug only)
@@ -487,13 +465,14 @@ W = {
     : W.next[item][property],
   
   // Transition an item
-  animation: (item, m = new DOMMatrix) =>
+  animation: (item, m = new DOMMatrix(W.next[item]?.M)) =>
     W.next[item]
-    ? m
-      .translateSelf(W.lerp(item, 'x'), W.lerp(item, 'y'), W.lerp(item, 'z'))
-      .rotateSelf(W.lerp(item, 'rx'),W.lerp(item, 'ry'),W.lerp(item, 'rz'))
-      .scaleSelf(W.lerp(item, 'w'),W.lerp(item, 'h'),W.lerp(item, 'd'))
-    : m,
+      ? m
+        .translateSelf(W.lerp(item, 'x'), W.lerp(item, 'y'), W.lerp(item, 'z'))
+        .rotateSelf(W.lerp(item, 'rx'), W.lerp(item, 'ry'), W.lerp(item, 'rz'))
+        .scaleSelf(W.lerp(item, 'w'), W.lerp(item, 'h'), W.lerp(item, 'd'))
+        .preMultiplySelf(W.animation(W.next[item].g))
+      : m,
     
   // Compute the distance squared between two objects (useful for sorting transparent items)
   dist: (a, b = W.next.camera) => a?.m && b?.m ? (b.m.m41 - a.m.m41)**2 + (b.m.m42 - a.m.m42)**2 + (b.m.m43 - a.m.m43)**2 : 0,
